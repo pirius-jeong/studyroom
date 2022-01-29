8bea71089a53bfc14a009955094dc06ddd62f09c
정영필 <pirius75@gmail.com>
2022-01-29 17:11:02 +0900

    220129-ALLOWD_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0jlbgqllwl5(t9(xmkx1afe8yva)6qj&au&x+u*18hdse=wjg('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [3.36.1.220]
 
 
 # Application definition
