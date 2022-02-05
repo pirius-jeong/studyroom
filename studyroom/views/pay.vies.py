ec8197f2b3160de2c885d80749148e0d5089756a
정영필 <pirius75@gmail.com>
2022-02-05 15:18:59 +0900

    pay_create_server

--- studyroom/views/pay.vies.py
+++ studyroom/views/pay.vies.py
@@ -5,33 +5,34 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.db.models import Q
 
-from ..models import Bill
+from ..models import Bill,Pay
 import logging
 logger = logging.getLogger('studyroom')
 
-def bill_list(request):
+def pay_list(request):
     logger.info("INFO 레벨로 출력")
     """
     학생 목록 출력
     """
     # 입력 파라미터
     page = request.GET.get('page', '1')  # 페이지
-    billmt = request.GET.get('billmt', '')  # 검색어
-    billstatus = request.GET.get('billstatus', '')  # 검색어
+    paymt = request.GET.get('paymt', '')  # 검색어
+    paystatus = request.GET.get('paystatus', '')  # 검색어
 
     # 조회
-    bill_list = Bill.objects.order_by('id')
-    if billmt:
-        bill_list = bill_list.filter(
-            Q(bill_mt__icontains=billmt)   # 제목검색
+    pay_list = Bill.objects.order_by('id')
+    if paymt:
+        pay_list = pay_list.filter(
+            Q(pay_mt__icontains=paymt)   # 제목검색
         ).distinct()
-    if billstatus:
-        bill_list = bill_list.filter(
-            Q(bill_status__icontains=billstatus)   # 제목검색
+    if paystatus:
+        pay_list = pay_list.filter(
+            Q(pay_status__icontains=paystatus)   # 제목검색
         ).distinct()
     # 페이징처리
-    paginator = Paginator(bill_list, 10)  # 페이지당 10개씩 보여주기
+    paginator = Paginator(pay_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {'bill_list': page_obj, 'page': page, 'billmt': billmt, 'billstatus':billstatus}
-    return render(request, 'studyroom/bill_list.html', context)
+    context = {'pay_list': page_obj, 'page': page, 'paymt': paymt, 'paystatus':paystatus}
+    return render(request, 'studyroom/pay_list.html', context)
+
