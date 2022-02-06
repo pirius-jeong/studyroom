106d765e8183348ff80c3f7f6c65f5f5cb586664
정영필 <pirius75@gmail.com>
2022-02-06 17:27:51 +0900

    virtualbox  ip

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,10 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.36.1.220', '10.0.2.15']
+# AWS
+#ALLOWED_HOSTS = ['3.36.1.220']
+
+# Virtualbox
+ALLOWED_HOSTS = ['10.0.2.15']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
