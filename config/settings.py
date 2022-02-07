3e3174b541ab5cac3a01527b06453927760ac0ea
정영필 <pirius75@gmail.com>
2022-02-07 20:51:23 +0900

    setting 디렉토리

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = '0jlbgqllwl5(t9(xmkx1afe8yva)6qj&au&x+u*18hdse=wjg('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['3.36.1.220']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
--- config/settings/prod.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = []
-DEBUG = False
