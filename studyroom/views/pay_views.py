56f0131ec16156ff2a1cb17418b06506eff8028c
pirius-jeong <32759593+pirius-jeong@users.noreply.github.com>
2022-02-10 10:06:42 +0900

    Update pay_views.py

--- studyroom/views/pay_views.py
+++ studyroom/views/pay_views.py
@@ -1,120 +1,29 @@
-{% extends 'base.html' %}
-{% load studyroom_filter %}
-{% block content %}
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from django.db.models import Q
 
-<div class="container my-3">
-    <div>
-        <h1>
-            <span class="badge badge-info">수납 목록</span>
-        </h1>
-    </div>
-    <div class="row">
-        <div class="col-xs-2">
-            <button class="btn " type="button" >이름</button>
-        </div>
-        <div class="col-xs-2">
-            <input id="input_search" type="text" class="form-control name" value="{{ name|default_if_none:'' }}">
-        </div>
-        <div class="col-xs-2">
-            <button class="btn btn-outline-secondary" type="button" id="btn_search">찾기</button>
-        </div>
-        <div class="col-xs-2">
-        </div>        
-    </div>
-    <table class="table">
-        <thead>
-        <tr class="text-center thead-light">
-            <th>수납일</th>
-            <th>구분</th>
-            <th>납부자</th>
-            <th>학생</th>
-            <th>수납금액</th>
-            <th>상태</th>
-        </tr>
-        </thead>
-        <tbody>
-        {% if pay_list %}
-        {% for pay in pay_list %}
-        <tr class="text-center">
-            <td>{{ pay.pay_date }}</td>
-            <td>{{ pay.pay_type }}</td>
-            <td>{{ pay.payer }}</td>
-            <td>{{ pay.account.student.name }}</td>
-            <td>{{ pay.pay_amt }}</td>
-            <td>{{ pay.pay_status }}</td>
-        </tr>
-        {% endfor %}
-        {% else %}
-        <tr>
-            <td colspan="3">수납 내역이 없습니다.</td>
-        </tr>
-        {% endif %}
-        </tbody>
-    </table>
-    <!-- 페이징처리 시작 -->
-    <ul class="pagination justify-content-center">
-        <!-- 이전페이지 -->
-        {% if pay_list.has_previous %}
-        <li class="page-item">
-            <a class="page-link" href="?page={{ pay_list.previous_page_number }}">이전</a>
-        </li>
-        {% else %}
-        <li class="page-item disabled">
-            <a class="page-link" tabindex="-1" aria-disabled="true" href="#">이전</a>
-        </li>
-        {% endif %}
-        <!-- 페이지리스트 -->
-        {% for page_number in pay_list.paginator.page_range %}
-        {% if page_number >= pay_list.number|add:-5 and page_number <= pay_list.number|add:5 %}
-            {% if page_number == pay_list.number %}
-            <li class="page-item active" aria-current="page">
-                <a class="page-link" href="?page={{ page_number }}">{{ page_number }}</a>
-            </li>
-            {% else %}
-            <li class="page-item">
-                <a class="page-link" href="?page={{ page_number }}">{{ page_number }}</a>
-            </li>
-            {% endif %}
-        {% endif %}
-        {% endfor %}
-        <!-- 다음페이지 -->
-        {% if pay_list.has_next %}
-        <li class="page-item">
-            <a class="page-link" href="?page={{ pay_list.next_page_number }}">다음</a>
-        </li>
-        {% else %}
-        <li class="page-item disabled">
-            <a class="page-link" tabindex="-1" aria-disabled="true" href="#">다음</a>
-        </li>
-        {% endif %}
-    </ul>
-    <!-- 페이징처리 끝 -->
+from ..models import Pay
+import logging
+logger = logging.getLogger('studyroom')
 
-</div>
-<form id="searchForm" method="get" action="{% url 'studyroom:pay_list' %}">
-    <input type="hidden" id="name" name="name" value="{{ name|default_if_none:'' }}">
-    <input type="hidden" id="page" name="page" value="{{ page }}">
-</form>
-{% endblock %}
-{% block script %}
-<script type='text/javascript'>
-$(document).ready(function(){
-    $(".page-link").on('click', function() {
-        $("#page").val($(this).data("page"));
-        $("#searchForm").submit();
-    });
+def pay_list(request):
+    logger.info("INFO 레벨로 출력")
+    """
+    수납 목록 출력
+    """
+    # 입력 파라미터
+    page = request.GET.get('page', '1')  # 페이지
+    name = request.GET.get('name', '')  # 검색어
 
-    $("#btn_search").on('click', function() {
-        $("#name").val($(".name").val());
-        $("#page").val(1);  // 검색버튼을 클릭할 경우 1페이지부터 조회한다.
-        $("#searchForm").submit();
-    });
-    $("#input_search").keydown(function(key) {
-        if (key.keyCode == 13) {
-            $("#name").val($(".name").val());
-            $("#page").val(1);  // 검색버튼을 클릭할 경우 1페이지부터 조회한다.
-            $("#searchForm").submit();
-    }});
-});
-</script>
-{% endblock %}
+    # 조회
+    pay_list = Pay.objects.order_by('id')
+    if name:
+        pay_list = pay_list.filter(
+            Q(account__student__name__icontains=name)   # 학생이름검색
+        ).distinct()
+    # 페이징처리
+    paginator = Paginator(pay_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'pay_list': page_obj, 'page': page, 'name': name}
+    return render(request, 'studyroom/pay_list.html', context)
