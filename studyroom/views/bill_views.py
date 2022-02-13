2aaec9c358645030b1fca8c0603e548c5d8e8658
정영필 <pirius75@gmail.com>
2022-02-13 10:16:51 +0900

    panas remove

--- studyroom/views/bill_views.py
+++ studyroom/views/bill_views.py
@@ -2,12 +2,6 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.db.models import Q
-from django.utils import timezone
-
-import sqlite3
-import pandas as pd
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 
 from studyroom.models import Bill
 
