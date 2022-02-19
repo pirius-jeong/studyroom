007b4ef561b383eecefa899cc766fcac3e601306
정영필 <pirius75@gmail.com>
2022-02-19 13:23:58 +0900

    a

--- studyroom/serializer.py
+++ studyroom/serializer.py
@@ -5,3 +5,11 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pay
         fields = ['pay_status', 'pay_type', 'pay_date', 'pay_amt', 'payer', 'account', 'create_date']
+
+    pay_type = serializers.CharField()
+    pay_date = serializers.CharField()
+    pay_amt = serializers.IntegerField()
+    payer = serializers.CharField()
+    pay_status = serializers.CharField()
+    account = serializers.PrimaryKeyRelatedField()
+    create_date = serializers.DateTimeField()
