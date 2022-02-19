24e849e72ba19fb1d20132613e9240ec63c32d6e
정영필 <pirius75@gmail.com>
2022-02-19 12:28:34 +0900

    d

--- config/urls.py
+++ config/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('studyroom/', include('studyroom.urls')),
     path('common/', include('common.urls')),
     path('', base_views.index, name='index'),  # '/' 에 해당되는 path
-    path('/post', base_views.post_api)
 ]
